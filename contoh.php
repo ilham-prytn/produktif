5a574fb70204cb7e852428a2f7e4a5213510913e
ilham-prytn <ahanprayitno@gmail.com>
2024-02-01 20:25:40 +0700

    second upload

--- contoh.php
+++ contoh.php
@@ -1,5 +1,23 @@
 <?php
-$nama = "Ilham";
-$usia = 18;
-$alamat = "Ambulu";
-echo $nama . "". $usia . "". $alamat . "";
+$x = 5985;
+var_dump($x);
+echo "<br>";
+$x = - 345; // negative number
+var_dump($x);
+echo "<br>";
+$x = 0x8C; // hexadecimal number
+var_dump($x);
+echo "<br>";
+$x = 047; // octal number
+var_dump($x);
+$x = 10.365;
+var_dump($x);
+echo "<br>";
+$x = 2.4e3;
+var_dump($x);
+echo "<br>";
+$x = 8E-5;
+var_dump($x);
+echo strlen("Hello world!");
+echo strpos("Hello world!","world");
+?>
